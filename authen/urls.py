34907e464ae3c8a9ebbca05b8f27519ace5c1a6d
mahdi2428 <mohammadmahdi836638@gmail.com>
2023-08-21 00:59:21 +0330

    this is for success

---
+++ authen/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from authen.views import MyTokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
